761620b874aeb7a551bbe995e997436248689d83
Arun-DevOfficial <darunkumar1204@gmail.com>
2024-07-23 22:02:17 +0530

    Added Project section

--- src/Page/Portfolio.jsx
+++ src/Page/Portfolio.jsx
@@ -1,22 +1,160 @@
-// import React from 'react'
+import Travel from "../assets/images/work-3.png";
+import Education from "../assets/images/work-2.png";
+import Blog from "../assets/images/work-4.png";
+import Restaurant from '../assets/images/Learn.png'
+import { BsGithub } from "react-icons/bs";
+import { BsBoxArrowUpRight } from "react-icons/bs";
 
 export default function Portfolio() {
   return (
     <>
       <section
-        className="w-full px-[12%] py-10 scroll-mt-20 h-screen"
+        className="w-full sm:px-[12%] py-12 scroll-mt-20 h-screen"
         id="work"
       >
         <h4 className="text-center mb-2 text-lg">What I Offer</h4>
-        <h1>Welcome to guvi</h1>
         <h2 className="text-center text-5xl">My Portfolio</h2>
         <p className="text-center max-w-2xl mx-auto mt-5 mb-12 p-3">
           Welcome to my web development portfolio! Explore a collection of
           projects showcasing my expertise in front-end development.
         </p>
-
-        <div className="grid grid-cols-1 sm:grid-cols-4 my-10 gap-5">
-          <div className="aspect-square bg-[url('')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer"></div>
+        <div className="grid gap-10 my-24 px-5 sm:px-10 md:px-20">
+          <div className="flex flex-col md:flex-row gap-4 sm:gap-6">
+            <img
+              src={Travel}
+              alt="Travel"
+              className="w-full md:w-1/2 border rounded-lg"
+            />
+            <div className="w-full md:w-1/2 max-w-lg text-center sm:text-start">
+              <h3 className="text-xl sm:text-2xl font-semibold my-4">
+                Trabook
+              </h3>
+              <p className="text-gray-600 my-5 leading-relaxed text-justify">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe
+                ullam iste deleniti aspernatur, autem expedita modi ipsa
+                exercitationem culpa id tempora neque est in magni! Dolore
+                voluptates est harum ipsam.
+              </p>
+              <div className="flex gap-5 items-center p-6">
+                <a
+                  href="#"
+                  className="flex items-center px-6 py-3 border rounded-full bg-gradient-to-r text-white font-semibold from-[#b820e6] to-[#da7d20] text-wrap gap-3 cursor-pointer"
+                >
+                  <BsGithub size={24} />
+                  <p>Github</p>
+                </a>
+                <a
+                  href="#"
+                  className="flex items-center gap-3 px-5 py-3 bg-white rounded-full text-wrap cursor-pointer border border-gray-500"
+                >
+                  <p className="font-medium text-gray-500">Preview</p>
+                  <BsBoxArrowUpRight size={20} className="text-gray-500" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row gap-4 sm:gap-6">
+            <img
+              src={Education}
+              alt="Education"
+              className="w-full md:w-1/2 border rounded-lg"
+            />
+            <div className="w-full md:w-1/2 max-w-lg text-center sm:text-start">
+              <h3 className="text-xl sm:text-2xl font-semibold my-4">
+                Thaai Food Restaurant
+              </h3>
+              <p className="text-gray-600 my-5 leading-relaxed text-justify">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe
+                ullam iste deleniti aspernatur, autem expedita modi ipsa
+                exercitationem culpa id tempora neque est in magni! Dolore
+                voluptates est harum ipsam.
+              </p>
+              <div className="flex gap-5 items-center p-6">
+                <a
+                  href="#"
+                  className="flex items-center px-6 py-3 border rounded-full bg-gradient-to-r text-white font-semibold from-[#b820e6] to-[#da7d20] text-wrap gap-3 cursor-pointer"
+                >
+                  <BsGithub size={24} />
+                  <p>Github</p>
+                </a>
+                <a
+                  href="#"
+                  className="flex items-center gap-3 px-5 py-3 bg-white rounded-full text-wrap cursor-pointer border border-gray-500"
+                >
+                  <p className="font-medium text-gray-500">Preview</p>
+                  <BsBoxArrowUpRight size={20} className="text-gray-500" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row gap-4 sm:gap-6">
+            <img
+              src={Blog}
+              alt="Blog"
+              className="w-full md:w-1/2 border rounded-lg"
+            />
+            <div className="w-full md:w-1/2 max-w-lg text-center sm:text-start">
+              <h3 className="text-xl sm:text-2xl font-semibold my-4">
+                Pixel Pluse
+              </h3>
+              <p className="text-gray-600 my-5 leading-relaxed text-justify">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe
+                ullam iste deleniti aspernatur, autem expedita modi ipsa
+                exercitationem culpa id tempora neque est in magni! Dolore
+                voluptates est harum ipsam.
+              </p>
+              <div className="flex gap-5 items-center p-6">
+                <a
+                  href="#"
+                  className="flex items-center px-6 py-3 border rounded-full bg-gradient-to-r text-white font-semibold from-[#b820e6] to-[#da7d20] text-wrap gap-3 cursor-pointer"
+                >
+                  <BsGithub size={24} />
+                  <p>Github</p>
+                </a>
+                <a
+                  href="#"
+                  className="flex items-center gap-3 px-5 py-3 bg-white rounded-full text-wrap cursor-pointer border border-gray-500"
+                >
+                  <p className="font-medium text-gray-500">Preview</p>
+                  <BsBoxArrowUpRight size={20} className="text-gray-500" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row gap-4 sm:gap-6">
+            <img
+              src={Restaurant}
+              alt="Travel"
+              className="w-full md:w-1/2 border rounded-lg"
+            />
+            <div className="w-full md:w-1/2 max-w-lg text-center sm:text-start">
+              <h3 className="text-xl sm:text-3xl font-semibold my-4">
+                Learn Mode
+              </h3>
+              <p className="text-gray-600 my-5 leading-relaxed text-justify">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe
+                ullam iste deleniti aspernatur, autem expedita modi ipsa
+                exercitationem culpa id tempora neque est in magni! Dolore
+                voluptates est harum ipsam.
+              </p>
+              <div className="flex gap-5 items-center p-6">
+                <a
+                  href="#"
+                  className="flex items-center px-6 py-3 border rounded-full bg-gradient-to-r text-white font-semibold from-[#b820e6] to-[#da7d20] text-wrap gap-3 cursor-pointer"
+                >
+                  <BsGithub size={24} />
+                  <p>Github</p>
+                </a>
+                <a
+                  href="#"
+                  className="flex items-center gap-3 px-5 py-3 bg-white rounded-full text-wrap cursor-pointer border border-gray-500"
+                >
+                  <p className="font-medium text-gray-500">Preview</p>
+                  <BsBoxArrowUpRight size={20} className="text-gray-500" />
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
